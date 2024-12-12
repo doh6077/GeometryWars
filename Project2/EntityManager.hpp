10925b85da3e18a8a5d86d3d180ba9ee7b0c09ab
doh6077 <kimdohee@sheridancollege.ca>
2024-12-12 16:47:14 -0500

    Create Entity and EntityManager classes

---
+++ Project2/EntityManager.hpp
@@ -0,0 +1,77 @@
+#pragma once
+
+#include "Entity.hpp"
+
+using ENtityVec = std::vector<std::shared_ptr<Entity>>;
+class EntityManager
+{
+	EntityVec m_entities;
+	EntityVec m_entitiesToAdd;
+	std::map<std::string, EntityVec> m_entityMap;
+	size_t    m_totalEntities = 0;
+
+	void removeDeadEntities(EntityVec& vec) {
+
+		// TODO: remove all dead entities from the input vector 
+		// This is called by the update() function 
+
+
+	}
+public:
+	EntityManager() = default; 
+	void update() {
+
+
+
+		// TODO: add entities from m_entitiesToAdd the proper location(s)
+		//       - add them to the vector of all entities 
+		//       - add them to the vector inside the map, with the tag as a key 
+
+		// remove dead entities from the vector of all entities 
+		removeDeadEntities(m_entities);
+
+		// remove dead entities from each vector in the entity map 
+		// C++20 way of iterating through [ key, value] pairs in a map 
+		for (auto& [tag, entityVec] : m_entityMap) {
+
+			removeDeadEntities(entityVec);
+		}
+	}
+
+	std::shared_ptr<Entity> addEntity(const std::string& tag) {
+
+		// create the entity shared pointer 
+		auto entity = std::shared_ptr<Entity>(new Entity(m_totalEntities++, tag));
+
+		// add it to the vec of all entities 
+		m_entitiesToAdd.push_back(entity);
+
+		// add it to the entity map 
+		if (m_entityMap.find(tag) == m_entityMap.end()) {
+			m_entityMap[tag] = EntityVec();
+		}
+		m_entityMap[tag].push_back(entity);
+
+
+		return entity; 
+
+
+	}
+
+	const EntityVec& getEntities()
+	{
+		return m_entities; 
+	}
+
+	const EntityVec& getEntities(const std::string& tag) {
+		
+		if (m_entityMap.find(tag) == m_entityMap.end()) {
+			m_entityMap[tag] = EntityVec();
+
+		}
+
+		return m_entityMap[tag];
+	
+	}
+
+};
