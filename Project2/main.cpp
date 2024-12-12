eeb926a305b56e12b665006fbd3c1ce6a8cc5d35
doh6077 <kimdohee@sheridancollege.ca>
2024-12-11 23:54:31 -0500

    Create Vec2 class with basic operations

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -2,12 +2,20 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <math.h>
+#include "vec2.hpp"
 int main() {
 
-	sf::Vector2f v1(16.5f, 24.f);
-	sf::Vector2f v2(16.5f, 24.f);
-	sf::Vector2f v3;
-	v3 = v1 + v2;
-	std::cout << v3.x;
+
+	// Test Vec2 class 
+	Vec2<int> v1(10, 20);
+	Vec2<int> v2(20, 30);
+
+	Vec2<int> v3 = v1 + v2; 
+	std::cout << v3.x << v3.y;
+	v1 += v2;
+	std::cout << v1.x << v1.y;
+	float distance = v1.dist(v2);
+	std::cout << distance; 
+
 	return 0;
 }
--- Project2/vec2.hpp
+++ Project2/vec2.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <math.h>
+#include <cmath>
 // By using the template class, the datatype wii be flexible 
 // Vec2<int> v; 
 // Vec2<float> v; 
@@ -27,58 +28,58 @@ public:
 	{
 	}
 
-	// allow automatic conversion to sf::Vector2
+	// allow automatic conversion from Vec2 to sf::Vector2
 	// this lets us pass Vec2 into sfml functions 
+	// if we used the customed vec2, it would have the compiler error 
 	operator sf::Vector2<T>()
 	{
 		return sf::Vector2<T>(x, y);
 	}
 
+	// overloading the opeartors 
 	Vec2 operator + (const Vec2& rhs) const
 	{
-		// ToDo 
-		return Vec2();
+	
+		return Vec2(x+ rhs.x, y + rhs.y);
 	}
 
 	Vec2 operator - (const Vec2& rhs) const
 	{
-		// ToDo 
-		return Vec2();
+		return Vec2(x - rhs.x, y - rhs.y);
 	}
 
 	Vec2 operator * (const Vec2& rhs) const
 	{
-		// ToDo 
-		return Vec2();
+		return Vec2(x * rhs.x, y * rhs.y);
 	}
 
-	Vec2 operator + (const Vec2& rhs) const
-	{
-		// ToDo 
-		return Vec2();
-	}
 	void operator += (const Vec2& rhs)
 	{
-
+		x += rhs.x;  // Add rhs.x to the current x component
+		y += rhs.y;  // Add rhs.y to the current y component
 	}
 	void operator -= (const Vec2& rhs)
 	{
-
+		x -= rhs.x;  
+		y -= rhs.y;  
 	}
 
 	void operator *= (const Vec2& rhs)
 	{
-
+		x *= rhs.x;
+		y *= rhs.y;
 	}
 
 	void operator /= (const Vec2& rhs)
 	{
-
+		x /= rhs.x;
+		y /= rhs.y;
 	}
 
 	float dist(const Vec2& rhs) const
 	{
-		// ToDo
+		float distance = sqrt(pow((x - rhs.x), 2) + pow((y - rhs.y), 2));
+		return distance; 
 	}
 };
 
