0dcb9848f5f3b865648019398b6955a0a3d2b211
doh6077 <kimdohee@sheridancollege.ca>
2024-12-16 22:52:20 -0500

    Add gitignore file

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -9,15 +9,15 @@ int main() {
 
 
 	// Test Vec2 class 
-	Vec2<int> v1(10, 20);
-	Vec2<int> v2(20, 30);
+	//Vec2<int> v1(10, 20);
+	//Vec2<int> v2(20, 30);
 
-	Vec2<int> v3 = v1 + v2; 
-	std::cout << v3.x << v3.y;
-	v1 += v2;
-	std::cout << v1.x << v1.y;
-	float distance = v1.dist(v2);
-	std::cout << distance; 
+	//Vec2<int> v3 = v1 + v2; 
+	//std::cout << v3.x << v3.y;
+	//v1 += v2;
+	//std::cout << v1.x << v1.y;
+	//float distance = v1.dist(v2);
+	//std::cout << distance; 
 
 
 	// Run the game 
